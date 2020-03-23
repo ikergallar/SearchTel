08cd94ffc711280983b4dee52e926e638fcebde4
ikergallar <ikergallar@192.168.1.130>
2020-03-23 11:25:20 +0100

    movidas

--- login.c
+++ login.c
@@ -4,70 +4,93 @@
  *  Created on: 22 mar. 2020
  *      Author: ikergallar
  */
-#include <stdio.h>
-#include <string.h> /* gets, strcmp */
-#include <stdlib.h> /* system */
+#include<stdio.h>
+#include <ctype.h>
 
-#define USUARIO "prueba1"
-#define CLAVE "prueba1"
+char username[]={0};
+char password[]={0};
+int main ()
+{  void sign_in();
+   void sign_up();
+   int selection=0;
+   printf("\n\t1>. Sign in\n\t2>. Sign up\n\tPlease make a selection\t");
+   scanf("%d",&selection);
+   switch(selection)
+      {
+      case 1:
+         sign_in();
+         break;
+      case 2:
+         sign_up();
+         break;
+      default:
+         printf("\n\tinvalid selection,try again\n");
+      }
 
-#define LONGITUD 80
 
-int main() {
-    char usuario[LONGITUD + 1];
-    char clave[LONGITUD + 1];
-    int intento = 0;
-    int ingresa = 0;
-    char caracter;
-    int i = 0;
+}
 
-    do {
-        i = 0;
-        system("cls");
-        printf("\n\t\t\tINICIO DE SESION\n");
-        printf("\t\t\t---------------\n");
-        printf("\n\tUSUARIO: ");
-        gets(usuario);
-        printf("\tCLAVE: ");
-        while (caracter = getch()) {
-            if (caracter == 13) {
-                clave[i] = '\0';
-                break;
+void sign_in()
+{  char user_name[]={'0'};
+   char pass_word[]={'0'};
+    int x,y,z,count=0;
+    retry:
+   printf("\n\tplease enter username:\t");
+   scanf("%s",user_name);
+   if(strcmp(username,user_name)==0)
+    {
+     printf("\tnow enter your password\t");
+     scanf("%s",pass_word);
+     if(strcmp(password,pass_word))
+      printf("\n\t\tyou're welcome!!");
+   else
+      {
+      printf("wrong password");
+      }
 
-            } else if (caracter == 8) {
-                if (i > 0) {
-                    i--;
-                    printf("\b \b");
-                }
+   }
 
-            } else {
-                if (i < LONGITUD) {
-                    printf("*");
-                    clave[i] = caracter;
-                    i++;
-                }
-            }
-        }
-
-        if (strcmp(usuario, USUARIO) == 0 && strcmp(clave, CLAVE) == 0) {
-            ingresa = 1;
-
-        } else {
-            printf("\n\tUsuario y/o clave son incorrectos\n");
-            intento++;
-            getchar();
-        }
-
-    } while (intento < 3 && ingresa == 0);
-
-    if (ingresa == 1) {
-        printf("\n\tBienvenido al Sistema\n");
-
-    } else {
-        printf("\n\tHa sobrepasado el numero maximo de intentos permitidos\n");
-    }
-
-    return 0;
+  else
+   {
+    printf("\tinvalid username");
+    main();
+   }
 }
 
 
+
+void sign_up()
+{
+   char confirmpswrd[10]={0};
+   int a=0;
+   int b=0;
+   int c=0;
+   printf("\tenter username\t");
+   scanf("%s",username);
+   reenterpswrd:
+   printf("\tplease enter password\t");
+   scanf("%s",password);
+    while(password[b]!='\0')
+      {
+         b++;
+      }
+   if(b<=7)
+   {
+      printf("\t\t***password too weak\ntry again\n");
+      goto reenterpswrd;
+   }
+   else if(b>=8&&b<=15)
+      printf("\t\t***strong password\n");
+   else
+      printf("\t\t***very strong password\n");
+   printf("\tconfirm password\t");
+   scanf("%s",confirmpswrd);
+   if(strcmp(password,confirmpswrd)==0)
+
+      printf("\n\tsign up successful!\n");
+
+   else if(c!=b)
+      printf("passwords don't match");
+
+
+}
