ca62a757989e75693954ed0a8f02d6c80a8195ea
alegr <alegr@DESKTOP-P1VUDJD>
2020-03-28 11:44:39 +0100

    aaaaaa

--- main.c
+++ main.c
@@ -1,69 +1,58 @@
+
+
+
 #include <stdio.h>
 #include <string.h>
+#include "Hotel.h"
+#include "Admin.h"
+#include "User.h"
 #include "registro.h"
 
-
+void home(){
+    char* name;
+    char c[2];
+    int check = 0;
+    printf("|---------- Welcome to booking hotel ----------|\n\n");
+    do{
+        int command = 0 ;
+        printf("1. Select By Hotel\n");
+        printf("2. Check Booking detail by name\n");
+        printf("3. Check Booking detail by phone number\n");
+        printf("4. Admin menu\n");
+        printf("5. Exit\n");
+        printf("Enter your Command :");
+        scanf("%s",&c);
+        seek_to_next_line();
+        command = atoi(c);
+        if(command > 5 || command < 0 || strlen(c) > 1){
+            command = 0 ;
+        }
+        switch (command){
+            case 1:
+                printf("\n");hotel();
+                break;
+            case 2:
+                printf("\n");search(0);
+                break;
+            case 3:
+                printf("\n");search(1);
+                break;
+            case 4:
+                printf("\n");login();  //Username  = Admin , Pass = rootAdmin
+                break;
+            case 5:
+                printf("\n");check = 1;
+                break;
+            default:
+                printf("Invalid command, please input again\n");
+        }
+    }while(check == 0 );
+    printf("Thank you for using program\n");
+}
 
 int main() {
-	printf("hola");
-	registrarUsuario();
-
-	/*
-    char usuario[LONGITUD + 1];
-
-    char clave[LONGITUD + 1];
-    int intento = 0;
-    int ingresa = 0;
-    char caracter;
-    int i = 0;
-
-    do {
-        i = 0;
-        system("cls");
-        printf("\n\t\t\tINICIO DE SESION\n");
-        printf("\t\t\t---------------\n");
-        printf("\n\tUSUARIO: ");
-        gets(usuario);
-        printf("\tCLAVE: ");
-        while (caracter = getch()) {
-            if (caracter == 13) {
-                clave[i] = '\0';
-                break;
-
-            } else if (caracter == 8) {
-                if (i > 0) {
-                    i--;
-                    printf("\b \b");
-                }
-
-            } else {
-                if (i < LONGITUD) {
-                    printf("*");
-                    clave[i] = caracter;
-                    i++;
-                }
-            }
-        }
-
-        if (strcmp(usuario, USUARIO) == 0 && strcmp(clave, CLAVE) == 0) {
-            ingresa = 1;
-
-        } else {
-            printf("\n\tUsuario y/o clave son incorrectos\n");
-            intento++;
-            getchar();
-        }
-
-    } while (intento < 3 && ingresa == 0);
-
-    if (ingresa == 1) {
-        printf("\n\tBienvenido al Sistema\n");
-
-    } else {
-        printf("\n\tHa sobrepasado el numero maximo de intentos permitidos\n");
-    }
-
-    return 0;
-    */
-	return 0;
+    home();
 }
+
+
+
